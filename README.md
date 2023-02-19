91bfedf5e1e0aab6e01c8bae7a27b9938360d536
yetone <yetoneful@gmail.com>
2023-02-19 15:22:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+OpenAI Translator Bob Plugin
+----------------------------
+
+# 简介
+
+ChatGPT 向我们展示了 GPT 模型的伟大之处，我使用 OpenAI 的 API 实现了这个翻译插件，虽然 OpenAI API 用的还是 GPT-3 模型（不是 ChatGPT 用的 GPT-3.5），但是其翻译效果已经很棒了。
+
+基于 ChatGPT 的 Bob 翻译插件正在开发中！
+
+# 使用截图
+
+
+# 使用方法
+
+1. 安装 [Bob](https://bobtranslate.com/guide/#%E5%AE%89%E8%A3%85) (版本 >= 0.50)
+2. 下载此插件: [openai-translator.bobplugin](https://github.com/yetone/bob-plugin-openai-translator/releases)
+3. 安装此插件:
+
+4. 去 [OpenAI](https://platform.openai.com/account/api-keys) 获取你的 API KEY
+5. 把 API KEY 填入 Bob 此插件配置界面的 API KEY
+ 输入框中
+
+6. 安装 [PopClip](https://bobtranslate.com/guide/integration/popclip.html) 实现划词后鼠标附近出现小图标
---
+++ src/main.js
@@ -0,0 +1,84 @@
+var items = [
+    ["auto", "auto"],
+    ["zh-Hans", "zh"],
+    ["zh-Hant", "zh"],
+    ["en", "en"],
+];
+
+var langMap = new Map(items);
+var langMapReverse = new Map(
+    items.map(([standardLang, lang]) => [lang, standardLang])
+);
+
+function supportLanguages() {
+    return items.map(([standardLang, lang]) => standardLang);
+}
+
+function translate(query, completion) {
+    const api_keys = $option.api_keys.split(",").map((key) => key.trim());
+    const api_key = api_keys[Math.floor(Math.random() * api_keys.length)];
+    const header = {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + api_key,
+    };
+    const body = {
+        model: $option.model,
+        prompt: `translate to ${langMap.get(query.detectTo)}:${query.text}`,
+        temperature: 0,
+        max_tokens: 4000,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+    };
+    (async () => {
+        const resp = await $http.request({
+            method: "POST",
+            url: "https://api.openai.com/v1/completions",
+            header,
+            body,
+        });
+
+        if (resp.error) {
+            const { statusCode } = resp.response;
+            let reason;
+            if (statusCode >= 400 && statusCode < 500) {
+                reason = "param";
+            } else {
+                reason = "api";
+            }
+            completion({
+                error: {
+                    type: reason,
+                    message: `接口响应错误 - ${response.data.msg}`,
+                    addtion: JSON.stringify(response),
+                },
+            });
+        } else {
+            const { choices } = resp.data;
+            if (!choices || choices.length === 0) {
+                completion({
+                    error: {
+                        type: "api",
+                        message: "接口未返回结果",
+                    },
+                });
+                return;
+            }
+            completion({
+                result: {
+                    from: query.detectFrom,
+                    to: query.detectTo,
+                    toParagraphs: choices[0].text.trim().split("\n"),
+                },
+            });
+        }
+    })().catch((err) => {
+        completion({
+            error: {
+                type: err._type || "unknown",
+                message: err._message || "未知错误",
+                addtion: err._addtion,
+            },
+        });
+    });
+}
