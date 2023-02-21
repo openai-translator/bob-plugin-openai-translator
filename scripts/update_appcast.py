7afdfbaa57cd2cf3d0d69c39373ff3d9d4324993
yetone <yetoneful@gmail.com>
2023-02-21 15:08:58 +0800

    feat: add appcast

---
+++ scripts/update_appcast.py
@@ -0,0 +1,33 @@
+import sys
+import hashlib
+import json
+from pathlib import Path
+
+
+def update_appcast(version):
+    release_file = Path(f'release/openai-translator-{version}.bobplugin')
+    assert release_file.is_file(), 'Release file not exist'
+    with open(release_file, 'rb') as f:
+        c = f.read()
+        file_hash = hashlib.sha256(c).hexdigest()
+    version_info = {
+        'version': version,
+        'desc': f'https://github.com/yetone/bob-plugin-openai-translator/releases/tag/v{version}',
+        'sha256': file_hash,
+        'url': f'https://github.com/yetone/bob-plugin-openai-translator/releases/download/v{version}/{release_file.name}',
+        'minBobVersion': '0.5.0'
+    }
+    appcast_file = Path('appcast.json')
+    if appcast_file.is_file():
+        with open(appcast_file, 'r') as f:
+            appcast = json.load(f)
+    else:
+        appcast = dict(identifier='yetone.openai.translator', versions=[])
+    appcast['versions'].insert(0, version_info)
+    with open(appcast_file, 'w') as f:
+        json.dump(appcast, f, ensure_ascii=False, indent=2)
+
+
+if __name__ == '__main__':
+    version = sys.argv[1]
+    update_appcast(version)
