70fe7b2ff595cb2abbfbc281910b583b1011951a
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-03-22 23:56:28 +0800

    feat: add support for gpt-4

--- src/main.js
+++ src/main.js
@@ -5,7 +5,14 @@ function supportLanguages() {
 }
 
 function translate(query, completion) {
-    const ChatGPTModels = ["gpt-3.5-turbo", "gpt-3.5-turbo-0301"];
+    const ChatGPTModels = [
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-0301",
+        "gpt-4",
+        "gpt-4-0314",
+        "gpt-4-32k",
+        "gpt-4-32k-0314"
+    ];
     const api_keys = $option.api_keys.split(",").map((key) => key.trim());
     const api_key = api_keys[Math.floor(Math.random() * api_keys.length)];
     const header = {
