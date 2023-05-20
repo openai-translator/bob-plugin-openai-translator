172729caf403ef4ef501d4fc3493d3c6296e09de
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-05-21 02:33:12 +0800

    chore: update the error handling logic

--- src/main.js
+++ src/main.js
@@ -12,6 +12,49 @@ var ChatGPTModels = [
 
 var SYSTEM_PROMPT = "You are a translation engine that can only translate text and cannot interpret it."
 
+var HttpErrorCodes = {
+    "400": "Bad Request",
+    "401": "Unauthorized",
+    "402": "Payment Required",
+    "403": "Forbidden",
+    "404": "Not Found",
+    "405": "Method Not Allowed",
+    "406": "Not Acceptable",
+    "407": "Proxy Authentication Required",
+    "408": "Request Timeout",
+    "409": "Conflict",
+    "410": "Gone",
+    "411": "Length Required",
+    "412": "Precondition Failed",
+    "413": "Payload Too Large",
+    "414": "URI Too Long",
+    "415": "Unsupported Media Type",
+    "416": "Range Not Satisfiable",
+    "417": "Expectation Failed",
+    "418": "I'm a teapot",
+    "421": "Misdirected Request",
+    "422": "Unprocessable Entity",
+    "423": "Locked",
+    "424": "Failed Dependency",
+    "425": "Too Early",
+    "426": "Upgrade Required",
+    "428": "Precondition Required",
+    "429": "Too Many Requests",
+    "431": "Request Header Fields Too Large",
+    "451": "Unavailable For Legal Reasons",
+    "500": "Internal Server Error",
+    "501": "Not Implemented",
+    "502": "Bad Gateway",
+    "503": "Service Unavailable",
+    "504": "Gateway Timeout",
+    "505": "HTTP Version Not Supported",
+    "506": "Variant Also Negotiates",
+    "507": "Insufficient Storage",
+    "508": "Loop Detected",
+    "510": "Not Extended",
+    "511": "Network Authentication Required"
+}
+
 /**
  * @param {string}  url
  * @returns {string} 
@@ -158,7 +201,6 @@ function buildRequestBody(model, isChatGPTModel, query) {
     };
 }
 
-
 /**
  * @param {Bob.TranslateQuery} query
  * @param {Bob.HttpResponse} result
@@ -170,12 +212,11 @@ function handleError(query, result) {
     query.onCompletion({
         error: {
             type: reason,
-            message: `接口响应错误 - ${result.data.error.message}`,
-            addtion: `${statusCode}: ${JSON.stringify(result)}`,
+            message: `接口响应错误 - ${HttpErrorCodes[statusCode]}`,
+            addtion: `${JSON.stringify(result)}`,
         },
     });
 }
-
 
 /**
  * @param {Bob.TranslateQuery} query
